369b2d71d804c72b619a6b6e339e0c9431600850
kk <k.klapczynski@gmail.com>
2019-11-18 14:20:55 +0100

    Basic directive created

---
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive({
+    selector:'[appBasicHighlight]'
+})
+export class BasicHighlightDirective implements OnInit {
+
+    constructor(private el: ElementRef){}
+
+    ngOnInit() {
+        this.el.nativeElement.style.backgroundColor = 'yellow';
+    }
+}
