936f1f008a1c009af87a37dfaffe5ad70e219f42
kk <k.klapczynski@gmail.com>
2019-11-19 14:44:47 +0100

    Enables external setting of directive property. Uses directive name as an alias for property. Omits [] in property binding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,26 +1,29 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
-  // better - more universal (for Service Workers e.g) approach is to not set style of a DOM element directly (as in basic-highlight), but using Renderer2
-  // constructor(private el: ElementRef, private renderer: Renderer2) { }
+export class BetterHighlightDirective implements OnInit {
+  // enable external setting of directive properies
+  @Input() defaultBackgroundColor: string = 'red';
+  // @Input() highlightBackgroundColor: string = 'blue';
+
+  // using directive name as an alias
+  @Input('appBetterHighlight') highlightBackgroundColor: string = 'blue';
+
+  constructor() { }
 
   ngOnInit() {
-    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'lightblue');
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 
   @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
-  // add interactivity: chnage background-color when hovering over element, that has directive as an attribute
   @HostListener('mousemove') onMouseEnter(eventData: Event) {
-    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'lightblue');
-    this.backgroundColor = 'lightblue';
+    this.backgroundColor = this.highlightBackgroundColor;
   }
 
   @HostListener('mouseout') onMouseLeave(eventData: Event) {
-    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultBackgroundColor;
   }
 }
