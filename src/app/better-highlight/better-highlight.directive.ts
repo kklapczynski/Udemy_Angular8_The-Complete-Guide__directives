13d9556bb89623c861e78b1b62d5281080c8ae90
kk <k.klapczynski@gmail.com>
2019-11-19 13:39:28 +0100

    Directive with Renderer2 and HostListener

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit{
+  // better - more universal (for Service Workers e.g) approach is to not set style of a DOM element directly (as in basic-highlight), but using Renderer2
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // this.renderer.setStyle(this.el.nativeElement, 'background-color', 'lightblue');
+  }
+
+  // add interactivity: chnage background-color when hovering over element, that has directive as an attribute
+  @HostListener('mousemove') onMouseEnter(eventData: Event) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'lightblue');
+  }
+  
+  @HostListener('mouseout') onMouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.el.nativeElement, 'background-color', 'transparent');
+  }
+}
