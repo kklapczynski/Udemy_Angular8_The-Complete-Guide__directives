e185b94877f741503f1e6c09e2472698c377fa21
kk <k.klapczynski@gmail.com>
2019-11-20 14:00:40 +0100

    Example of ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,22 @@ export class AppComponent {
   oddNumbers = [1,3,5];
   evenNumbers = [2,4];
   onlyOdd = false;
+  values = [1, 3, 5, 7, 10, 13, 15, 50, 100];
+  value: number = this.values[0];
+  previousValueIndex = -1;
+
+  getCurrentValueIndex(value: number) {
+    return this.values.indexOf(value);
+  }
+
+  changeValue(currentValue: number) {
+    let currentValueIndex = this.getCurrentValueIndex(currentValue);
+    
+    if (currentValueIndex === 0 || (currentValueIndex < this.values.length -1 && this.previousValueIndex < currentValueIndex)) {
+      this.previousValueIndex = currentValueIndex;
+      this.value = this.values[++currentValueIndex];
+    } else {
+      this.value = this.values[--currentValueIndex];
+    }
+  }
 }
